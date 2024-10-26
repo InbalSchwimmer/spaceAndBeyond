1e464ec2dfda82d1434062c85c40627f4f0378c5
InbalSchwimmer <inbalschwimmer@gmail.com>
2024-10-26 21:56:43 +0300

    add new scenarios

--- tests/checkout_test.py
+++ tests/checkout_test.py
@@ -12,13 +12,47 @@ class TestCheckout:
     @pytest.mark.functional
     @pytest.mark.regression
     @allure.severity(Severity.BLOCKER)
-    @allure.description("This test verifies that click on book a flight button after filling in the flight details "
-                        "will display checkout section by verifying url")
-    @allure.title("Verify flight booking redirects to checkout")
+    @allure.description("This test verifies that numbers of travelers that was booked to the flight display in "
+                        "checkout section")
+    @allure.title("Verify numbers of travelers in checkout")
     def test_num_of_travelers(self):
-        user = Checkout(self.driver)
-        flight = HomePage(self.driver)
-        with allure.step("Select destination"):
-            flight.scroll_to_element_and_click(HomePage.BOOK_BABAHOYO_BTN)
-        user.fill_text(user.NAME_TEXT_FILED, "Inbal")
-        time.sleep(10)
+        book_flight = HomePage(self.driver)
+        with allure.step("Select departure date"):
+            book_flight.click_on_departure_picker()
+            book_flight.select_departure_date()
+            book_flight.click(HomePage.CALENDER_OK_BTN)
+        with allure.step("Select returning date"):
+            book_flight.click_on_returning_picker()
+            book_flight.select_returning_date()
+            book_flight.click(HomePage.CALENDER_OK_BTN)
+        with allure.step("Check the order price"):
+            book_flight.fill_travel_data(3, 2, "blue", 1000, HomePage.BOOK_BABAHOYO_BTN)
+        with allure.step("Assert number of travelers as was entered"):
+            user = Checkout(self.driver)
+            num_of_travelers = user.get_text(user.NUMBER_OF_TRAVELERS)
+            assert num_of_travelers == "5 travelers"
+
+    @pytest.mark.regression
+    @allure.severity(Severity.BLOCKER)
+    @allure.description("This test verifies that the total price of the order is as expected by calculating ticket "
+                        "price * number of travelers")
+    @allure.title("Verify order price as expected")
+    def test_total_order_price(self):
+        book_flight = HomePage(self.driver)
+        with allure.step("Select departure date"):
+            book_flight.click_on_departure_picker()
+            book_flight.select_departure_date()
+            book_flight.click(HomePage.CALENDER_OK_BTN)
+        with allure.step("Select returning date"):
+            book_flight.click_on_returning_picker()
+            book_flight.select_returning_date()
+            book_flight.click(HomePage.CALENDER_OK_BTN)
+        with allure.step("Fill order details"):
+            book_flight.fill_travel_data(3, 2, "blue", 1000, HomePage.BOOK_BABAHOYO_BTN)
+        with allure.step("Check the order price"):
+            user = Checkout(self.driver)
+            expected_price = user.calculate_price_order(5, "babahoyo")
+            order_price = user.get_text(user.ORDER_TOTAL_SUM)
+            actual_price = user.price_without_signs(order_price)
+        with allure.step("Assert actual_price is equal to expected_price"):
+            assert actual_price == expected_price
